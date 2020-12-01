b3ffe5dd2be672d6baaf282b53c20093520c369c
MaxKayan <maxim.kayander1@gmail.com>
2020-12-01 16:54:59 +0300

    Adding Visitor receiver

---
+++ web/receivers.py
@@ -0,0 +1,9 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from web.models import Visitor
+
+
+@receiver(post_save, sender=Visitor)
+def update_device_data(instance: Visitor, created: bool, raw: bool, **kwargs):
+    print(instance, created, raw)
