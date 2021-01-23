9288c6e989b774e62ad98750f867a0f98de9765e
MaxKayan <maxim.kayander1@gmail.com>
2021-01-24 00:44:31 +0300

    Corrected gunicorn config

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -3,6 +3,8 @@ pythonpath = '/home/www/mobile-repair-cloud/project/'
 bind = 'unix:/home/www/mobile-repair-cloud/cloud.sock'
 workers = 5
 user = 'www'
+# group = 'www-data'
+# error-logfile = '-'
 limit_request_fields = 32000
 limit_request_field_size = 0
 raw_env = 'DJANGO_SETTINGS_MODULE=project.settings-prod'
