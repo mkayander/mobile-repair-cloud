e5aef7831540891b01bbf0cc2bfbc5fd280c8a97
MaxKayan <maxim.kayander1@gmail.com>
2021-01-23 23:53:17 +0300

    Added gunicorn python dependency

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'r+b!)8o!7ahd-i1&@7t!xq^x&kgl)n@6$^&)%#u7pt9m)pvzq('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.0.186', '10.36.240.155']
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.0.186', '10.36.240.155', 'cloudsc.ru']
 
 # Application definition
 
