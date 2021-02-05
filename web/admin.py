49055d6bff4c81ac89f0b8639280588a8b3b587e
MaxKayan <maxim.kayander1@gmail.com>
2021-02-05 16:54:09 +0300

    Modifed visitors list display

--- web/admin.py
+++ web/admin.py
@@ -6,6 +6,7 @@ from web.models import Visitor, FeedbackRequest, GalleryPhoto
 @admin.register(Visitor)
 class VisitorAdmin(admin.ModelAdmin):
     list_display = ["ip_address", "session", "user_agent", "visit_count", "last_visit", "created_at"]
+    ordering = ["-last_visit"]
 
 
 @admin.register(FeedbackRequest)
