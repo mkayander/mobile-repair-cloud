af438b54804d8dd237aefa3ddb4492d2fa4e87ba
MaxKayan <maxim.kayander1@gmail.com>
2021-02-07 23:52:10 +0300

    Added a solution to serve a specific static file at website's root, instead of at a '/static/' url branch. PWA Builder and pwa-update js code specifically expects the 'pwabuilder-sw.js' file to be at website's root url, with no option to change that.

--- web/urls.py
+++ web/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_page),
+    path('pwabuilder-sw.js', views.StaticFileView.as_view(source="js\\pwa.js"))
     # path('api/', include('api.urls'))
 ]
--- web/views.py
+++ web/views.py
@@ -1,8 +1,16 @@
+import mimetypes
+import os
+
 from constance import config
-from django.http import HttpRequest
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, Http404, HttpResponseNotModified
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.utils.http import http_date
+from django.views import View
+from django.views.static import was_modified_since
+
+from project import settings
 from web.models import Visitor
 
 
@@ -32,3 +40,29 @@ def landing_page(request: HttpRequest):
     visitor.save()
 
     return render(request, "landing_page.html", {"config": config})
+
+
+def js_file(request: HttpRequest):
+    return HttpResponse("parent.Response_OK()", mimetype="application/x-javascript")
+
+
+class StaticFileView(View):
+    http_method_names = ['get']
+    source = None
+
+    def get(self, request, *args, **kwargs):
+        path = os.path.join(settings.STATIC_ROOT, self.source)
+        if not os.path.exists(path):
+            raise Http404('"%s" does not exist' % path)
+        stat = os.stat(path)
+        mimetype, encoding = mimetypes.guess_type(path)
+        mimetype = mimetype or 'application/octet-stream'
+        if not was_modified_since(request.META.get('HTTP_IF_MODIFIED_SINCE'),
+                                  stat.st_mtime, stat.st_size):
+            return HttpResponseNotModified(mimetype - mimetype)
+        response = HttpResponse(open(path, 'rb').read(), content_type=mimetype)
+        response['Last-Modified'] = http_date(stat.st_mtime)
+        response['Content-Length'] = stat.st_size
+        if encoding:
+            response['Content-Encoding'] = encoding
+        return response
