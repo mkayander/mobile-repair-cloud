5b46a85fe346455f6459aafb4fdba0b233f907d0
MaxKayan <maxim.kayander1@gmail.com>
2021-02-08 11:13:53 +0300

    Removed STATICFILES_DIRS setting, as it is redundant in this case and causes warnings.

--- project/settings.py
+++ project/settings.py
@@ -137,11 +137,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    # BASE_DIR / "static",
-    BASE_DIR / "web/static",
-]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = "/media/"
