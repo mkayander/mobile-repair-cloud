3beede7c123ed96baf91f845a266e362997a4949
MaxKayan <maxim.kayander1@gmail.com>
2021-02-08 11:14:41 +0300

    Made static file path crossplatform using 'os' package.

--- web/urls.py
+++ web/urls.py
@@ -1,9 +1,11 @@
+import os
+
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('', views.landing_page),
-    path('pwabuilder-sw.js', views.StaticFileView.as_view(source="js/pwa.js"))
+    path('pwabuilder-sw.js', views.StaticFileView.as_view(source=os.path.join("js", "pwa.js")))
     # path('api/', include('api.urls'))
 ]
