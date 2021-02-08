abecd9ffeecab07664c78baa718d153ab2fb4ed9
MaxKayan <maxim.kayander1@gmail.com>
2021-02-08 10:07:39 +0300

    Fixed the path slash character being false for the UNIX systems

--- web/urls.py
+++ web/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_page),
-    path('pwabuilder-sw.js', views.StaticFileView.as_view(source="js\\pwa.js"))
+    path('pwabuilder-sw.js', views.StaticFileView.as_view(source="js/pwa.js"))
     # path('api/', include('api.urls'))
 ]
