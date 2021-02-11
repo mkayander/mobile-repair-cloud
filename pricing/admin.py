cf5af5833360abd6c4177a4b802b835fc268895b
MaxKayan <maxim.kayander1@gmail.com>
2021-02-11 17:05:55 +0300

    Added the basic admin classes for the models

--- pricing/admin.py
+++ pricing/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from pricing.models import Brand, DeviceModel, Service
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(DeviceModel)
+class DeviceModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    pass
