9f53fdf9c2c4a3f97b91113b98e0efd9b7bc763d
MaxKayan <maxim.kayander1@gmail.com>
2021-02-11 16:50:25 +0300

    Added a new app with pricing models and new dependency - djmoney

---
+++ pricing/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+from djmoney.models.fields import MoneyField
+
+
+class Brand(models.Model):
+    name = models.TextField(verbose_name="Наименование", max_length=64)
+
+    class Meta:
+        verbose_name = "Брэнд устройства"
+        verbose_name_plural = "Брэнды устройств"
+
+
+class DeviceModel(models.Model):
+    name = models.TextField(verbose_name="Наименование", max_length=64)
+    brand = models.ForeignKey(Brand, verbose_name="Брэнд", on_delete=models.CASCADE, related_name="models")
+
+    class Meta:
+        verbose_name = "Модель устройства"
+        verbose_name_plural = "Модели устройств"
+
+
+class Service(models.Model):
+    name = models.TextField(verbose_name="Наименование", max_length=128)
+    model = models.ForeignKey(DeviceModel, verbose_name="Модель устройства", on_delete=models.CASCADE,
+                              related_name="services")
+    price = MoneyField(verbose_name="Стоимость", max_digits=8, decimal_places=2, default_currency='RUB', null=True)
+
+    class Meta:
+        verbose_name = "Услуга"
+        verbose_name_plural = "Услуги"
