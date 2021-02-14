0b308dbfb4af9e415e8c29d346e3902f61fb2bc5
MaxKayan <maxim.kayander1@gmail.com>
2021-02-14 19:42:10 +0300

    A proper import

--- pricing/validators.py
+++ pricing/validators.py
@@ -1,4 +1,4 @@
-import xml.etree.ElementTree as ET
+from xml.etree import ElementTree
 
 from django.core.exceptions import ValidationError
 from django.db.models.fields.files import FieldFile
@@ -6,6 +6,6 @@ from django.db.models.fields.files import FieldFile
 
 def validate_svg(file: FieldFile):
     target_tag = '{http://www.w3.org/2000/svg}svg'
-    file_tag = ET.fromstring(file.read()).tag
+    file_tag = ElementTree.fromstring(file.read()).tag
     if not file_tag == target_tag:
         raise ValidationError("The file is not an SVG")
