84078fb3dffd1eee55be9de992b3af240b5604d2
MaxKayan <maxim.kayander1@gmail.com>
2021-02-14 19:41:40 +0300

    Added svg logo display to the admin

--- pricing/admin.py
+++ pricing/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 # Register your models here.
+from imagekit.admin import AdminThumbnail
+
 from pricing.models import Brand, DeviceModel, Service
 
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
-    pass
+    image_display = AdminThumbnail(image_field='logo', template="admin_thumbnail.html")
+    image_display.short_description = _('Logo display')
+
+    list_display = ['name', 'image_display']
+    readonly_fields = ['image_display']  # this is for the change form
 
 
 @admin.register(DeviceModel)
