2d11470413c9f006d97969f7079c49674b54810f
MaxKayan <maxim.kayander1@gmail.com>
2021-02-14 18:44:31 +0300

    Fixed (rewrote) the svg file validator

--- pricing/validators.py
+++ pricing/validators.py
@@ -1,20 +1,11 @@
-import xml.etree.cElementTree as et
+import xml.etree.ElementTree as ET
 
 from django.core.exceptions import ValidationError
+from django.db.models.fields.files import FieldFile
 
 
-def is_svg(filename):
-    tag = None
-    with open(filename, "r") as f:
-        try:
-            for event, el in et.iterparse(f, ('start',)):
-                tag = el.tag
-                break
-        except et.ParseError:
-            pass
-    return tag == '{http://www.w3.org/2000/svg}svg'
-
-
-def validate_svg(file, is_valid):
-    if not is_svg(file):
+def validate_svg(file: FieldFile):
+    target_tag = '{http://www.w3.org/2000/svg}svg'
+    file_tag = ET.fromstring(file.read()).tag
+    if not file_tag == target_tag:
         raise ValidationError("The file is not an SVG")
