c4eaf677e0a89e77c8b82b01209b5e40237091e5
MaxKayan <maxim.kayander1@gmail.com>
2021-02-15 16:49:56 +0300

    Added try-except so the failed validation is displayed correctly

--- pricing/validators.py
+++ pricing/validators.py
@@ -1,11 +1,18 @@
 from xml.etree import ElementTree
+from xml.etree.ElementTree import ParseError
 
 from django.core.exceptions import ValidationError
 from django.db.models.fields.files import FieldFile
+from django.utils.translation import ugettext_lazy as _
 
 
 def validate_svg(file: FieldFile):
     target_tag = '{http://www.w3.org/2000/svg}svg'
-    file_tag = ElementTree.fromstring(file.read()).tag
+
+    try:
+        file_tag = ElementTree.fromstring(file.read()).tag
+    except ParseError:
+        file_tag = None
+
     if not file_tag == target_tag:
-        raise ValidationError("The file is not an SVG")
+        raise ValidationError(_("The file is not an SVG"))
